34f122e901ae22ea0c07417f7e6e47ef0fe5e73e
shubhaannsaae"n <shubhaannsaae5@gmail.com"n>
2024-10-03 21:45:17 +0530

    Fund Me via Foundry

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,56 @@
+//1. Deploy mocks when we are on a local anvil chain
+//2. Keep track of contract addresses across differnt chains
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // If we are on a local anvil chain, deploy mocks
+    // Otherwise, grab the existing contract addresses from live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; // Sepolia ETH/USD price feed address
+    }
+
+    constructor() {
+        if(block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaETHConfig();
+        } else{
+            activeNetworkConfig = getOrCreateAnvilETHConfig();
+        }
+    }
+
+    function getSepoliaETHConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed : 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilETHConfig() public returns (NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy mocks
+        // 2. Return the mock addresses
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE); 
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed : address(mockPriceFeed)
+        });
+
+        return anvilConfig;
+    }
+}
